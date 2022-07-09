7a1cf5870a59ebcbf39039bedfee89fc5ede9991
dhirajpatil2346 <79779312+dhirajpatil2346@users.noreply.github.com>
2022-07-09 22:11:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
